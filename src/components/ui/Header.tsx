14017675e08c8b2b4692af057e2a1d27c571bdcd
Rastislav Wagner <rawagner@redhat.com>
2020-05-18 20:19:59 +0200

    Add pre-alpha badge

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Brand, PageHeader } from '@patternfly/react-core';
+import { Brand, PageHeader, Label } from '@patternfly/react-core';
 import logo from '../../images/metal3_facet-whitetext.svg';
+
+import './header.scss';
 
 const Header: React.FC = () => (
   <PageHeader
     logo={<Brand src={logo} alt="Metal&sup3; Facet UI" />}
-    // toolbar={PageToolbar}
+    toolbar={<Label className="header-badge">Pre-alpha</Label>}
     //avatar={<Avatar src={avatarImg} alt="Avatar image" />}
     showNavToggle
     // onNavToggle={this.onNavToggle}
